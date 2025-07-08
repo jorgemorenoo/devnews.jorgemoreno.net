e3b2ef36b0d009aa0d0ed7317974c5a5efe49ddc
Jorge Moreno <morenojorgeaugusto@gmail.com>
2025-07-07 22:39:15 -0300

    alterando o arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home () {
   return (
     <div>
-      <h1>Bem-vindo ao DevNews</h1>
+      <h1>Bem-vindo ao DevNews do Jorge Moreno</h1>
       <p>Este é um repositório onde estou desenvolvendo minhas habilidades em programação na web.</p>
       <p>Use o comando <code>npm run dev</code> para iniciar o servidor de desenvolvimento do Next.js.</p>
     </div>
