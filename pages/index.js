e57575b6bf0d49f6e6bb9dfed3e7ba5203cb4978
Jorge Moreno <morenojorgeaugusto@gmail.com>
2025-07-07 21:37:14 -0300

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+  return (
+    <div>
+      <h1>Bem-vindo ao DevNews</h1>
+      <p>Este é um repositório onde estou desenvolvendo minhas habilidades em programação na web.</p>
+      <p>Use o comando <code>npm run dev</code> para iniciar o servidor de desenvolvimento do Next.js.</p>
+    </div>
+  );
+}
+export default Home;
