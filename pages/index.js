3015118a919d858670a867cdb005c2162f6b4587
Jorge Moreno <morenojorgeaugusto@gmail.com>
2025-08-01 18:19:09 -0300

    Acrescentando uma nova frase

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home () {
       <h1>Bem-vindo ao DevNews do Jorge Moreno</h1>
       <p>Este é um repositório onde estou desenvolvendo minhas habilidades em programação na web.</p>
       <p>Use o comando correto <code>npm run dev</code> para iniciar o servidor de desenvolvimento do Next.js.</p>
+      <p>Fazendo um novo commit</p>
     </div>
   );
 }
